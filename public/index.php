7c3fdccb0a00bcfaed5630b8c96ef0a4c81d80ba
J9-tops <j9.tops@gmail.com>
2025-10-27 23:04:40 +0000

    feat: add all pages

--- public/index.php
+++ public/index.php
@@ -1,11 +1,33 @@
 <?php
-session_start();
-$currentUser = $_SESSION['ticketapp_session'] ?? null;
-
 require_once __DIR__ . '/../vendor/autoload.php';
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
-$twig = new \Twig\Environment($loader);
 
-echo $twig->render('landing.html.twig', [
-    'currentUser' => $currentUser
-]);
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+$twig = new \Twig\Environment($loader, ['cache' => false]);
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($path) {
+  case '/':
+    echo $twig->render('landing.html.twig');
+    break;
+
+  case '/sign-up':
+    echo $twig->render('sign-up.html.twig');
+    break;
+
+  case '/sign-in':
+    echo $twig->render('sign-in.html.twig');
+    break;
+
+  case '/dashboard':
+    echo $twig->render('dashboard.html.twig');
+    break;
+  case '/dashboard/tickets':
+    echo $twig->render('tickets.html.twig');
+    break;
+
+  default:
+    http_response_code(404);
+    echo $twig->render('not-found.html.twig');
+    break;
+}
